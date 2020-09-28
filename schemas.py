accc1fd27cdd2c50362926397039e168c56c722f
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-28 18:26:59 +0530

    Pydantic models

---
+++ schemas.py
@@ -0,0 +1,27 @@
+from typing import List
+from pydantic import BaseModel
+ 
+class ItemBase(BaseModel):
+    title: str
+    price: int
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+    class Config:
+        orm_mode=True
+
+class UserBase(BaseModel):
+    email: str
+class UserCreate(UserBase):
+    password: str
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item]=[]
+    class Config:
+        orm_mode=True
+        
