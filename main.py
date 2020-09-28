628a5db78ac93d801ef19c0aa5f23f9802452200
ATRI2107 <atrichaturvedi@gmail.com>
2020-09-28 21:50:30 +0530

    Added routes for the api

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import crud
+import models
+import schemas
+
+models.Base.metadata.create_all(bind=engine)
+app=FastAPI()
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users/")
+def create_user(user: schemas.UserCreate,db:Session=Depends(get_db)):
+    db_user=crud.get_user_by_email(db,email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400,detail="Email already Exists")
+    return crud.create_user(db=db,user=user)
+
+@app.get("/users/")
+def get_users(skip: int =0, limit: int=100, db:Session=Depends(get_db)):
+    users=crud.get_users(db,skip=skip,limit=limit)
+    return users
+
+@app.get("/users/{user_id}")
+def read_a_user(user_id: int, db: Session=Depends(get_db)):
+    db_user=crud.get_user(db,user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=400,detail="User Does Not Exists")
+    return db_user
+
+@app.post("/users/{user_id}/items/")
+def create_item_for_user(user_id: int,item: schemas.ItemCreate,db: Session=Depends(get_db)):
+    return crud.create_user_item(db=db,item=item,user_id=user_id)
+
+@app.get("/users/{user_id}/item/")
+def get_item_for_user(user_id: int , db: Session=Depends(get_db)):
+    return crud.get_items(db=db,user_id=user_id)
